049876049ab060a6c37c6cfb2d43ff7594a0f8f0
Leandro Rey <leandrodrey@gmail.com>
2023-05-15 22:49:21 -0300

    Page deleted

--- ecommerce/src/routes/SiteRoutes.jsx
+++ ecommerce/src/routes/SiteRoutes.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MainLayout from "./MainLayout";
 import Home from "../pages/Home";
-import Store from "../pages/Store";
 import AboutUs from "../pages/AboutUs";
 import ContactUs from "../pages/ContactUs";
 import ItemDetail from "../pages/ItemDetail";
@@ -14,8 +13,7 @@ const SiteRoutes = () => {
             <Routes>
                 <Route path='/' element={<MainLayout/>}>
                     <Route index element={<Home/>} />
-                    <Route path='/store' element={<Store/>} />
-                    <Route path='/store/category/:id' element={<Store/>} />
+                    <Route path='/category/:id' element={<Home/>} />
                     <Route path='/aboutus' element={<AboutUs/>} />
                     <Route path='/contactus' element={<ContactUs/>} />
                     <Route path='/item/:id' element={<ItemDetail/>} />
