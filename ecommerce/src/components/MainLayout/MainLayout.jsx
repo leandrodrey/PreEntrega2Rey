686958a512a238216390247b0b80e47c454fcfd0
leandrodrey <leandrodrey@gmail.com>
2023-05-28 22:13:51 -0300

    Loader deleted from Main Layout

--- ecommerce/src/components/MainLayout/MainLayout.jsx
+++ ecommerce/src/components/MainLayout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -10,7 +10,6 @@ import Container from "@mui/material/Container";
 import imgLogo from '../NavBar/images/logo.png';
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
-import Loader from "../Loader/Loader";
 import ProductProvider from "../../context/ProductProvider";
 import CartProvider from "../../context/CartProvider";
 
@@ -62,39 +61,25 @@ const menuItems = [
 
 const MainLayout = () => {
 
-    const [loader, setLoader] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoader(false);
-        }, 2000);
-    }, []);
-
     return (
         <StyledEngineProvider injectFirst>
             <ThemeProvider theme={siteTheme}>
                 <CssBaseline />
-                { loader ?
-                    <Loader showLoader={true} />
-                :
-                    <React.Fragment>
-                        <ProductProvider>
-                            <CartProvider>
-                                <header>
-                                    <NavBar
-                                        title={siteInfo.name}
-                                        logo={siteInfo.logo}
-                                        menuItems={menuItems}
-                                    />
-                                </header>
-                                <Container maxWidth="xl">
-                                    <Outlet />
-                                </Container>
-                                <Footer />
-                            </CartProvider>
-                        </ProductProvider>
-                    </React.Fragment>
-                }
+                <ProductProvider>
+                    <CartProvider>
+                        <header>
+                            <NavBar
+                                title={siteInfo.name}
+                                logo={siteInfo.logo}
+                                menuItems={menuItems}
+                            />
+                        </header>
+                        <Container maxWidth="xl">
+                            <Outlet />
+                        </Container>
+                        <Footer />
+                    </CartProvider>
+                </ProductProvider>
             </ThemeProvider>
         </StyledEngineProvider>
     )
