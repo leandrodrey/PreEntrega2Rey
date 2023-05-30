d36315c511439a5342a84ed4fdee35611b404b42
Leandro Rey <leandrodrey@gmail.com>
2023-05-30 00:42:46 -0300

    Product Provider updated

--- ecommerce/src/context/ProductProvider.jsx
+++ ecommerce/src/context/ProductProvider.jsx
@@ -1,22 +1,17 @@
-import React, {createContext, useEffect, useMemo} from 'react'
+import React, {createContext} from 'react'
 import useFirebase from "../hooks/useFirebase";
 
 export const ProductContext = createContext('')
 
 const ProductProvider = ({children}) => {
 
-    const {products, getProducts} = useFirebase();
-    const value = useMemo(() => ({products}), [products]);
-
-    useEffect(() => {
-        getProducts();
-    },[getProducts]);
+    const {products, getProducts, handleSubmit} = useFirebase();
 
     return (
-        <ProductContext.Provider value={value}>
+        <ProductContext.Provider value={{products, getProducts, handleSubmit}}>
             {children}
         </ProductContext.Provider>
     )
 
 }
-export default ProductProvider
+export default ProductProvider;
