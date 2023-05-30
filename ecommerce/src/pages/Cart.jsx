5476d249f817735577b686ffeefbdb6c71f8cf1f
Leandro Rey <leandrodrey@gmail.com>
2023-05-30 00:38:38 -0300

    Cart View first approach

--- ecommerce/src/pages/Cart.jsx
+++ ecommerce/src/pages/Cart.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Form from "../components/Form/Form";
+import {CartContext} from "../context/CartProvider";
+import {ProductContext} from "../context/ProductProvider";
 
 const Cart = () => {
+
+    const {cart, total} = useContext(CartContext);
+    const {handleSubmit} = useContext(ProductContext);
+
     return (
-        <div>Cart</div>
+        <Box>
+            <Typography variant="h3" gutterBottom color="text.secondary">
+                Cart
+            </Typography>
+            <Typography variant="h5" gutterBottom color="text.secondary">
+                {cart.map(item =>
+                    <li key={item.title}>
+                        {item.title} - Cantidad: {item.count}
+                    </li>
+                )}
+                Total: ${total}
+            </Typography>
+            <Form total={cart.length} items={cart} handleSubmit={handleSubmit}/>
+        </Box>
     )
 }
-export default Cart
+export default Cart;
