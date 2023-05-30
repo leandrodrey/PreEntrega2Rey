2cbe70cc1001815a3d209a96f3f13cf35b0d4b20
Leandro Rey <leandrodrey@gmail.com>
2023-05-30 00:42:08 -0300

    handleSubmit added

--- ecommerce/src/hooks/useFirebase.jsx
+++ ecommerce/src/hooks/useFirebase.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import {collection, getDocs} from "firebase/firestore";
+import {useState} from 'react'
+import {collection, getDocs, addDoc} from "firebase/firestore";
 import {db} from "../services/firebase.config";
 import item from "../items.json";
 
@@ -8,11 +8,13 @@ const UseFirebase = () => {
     const [products, setProducts] = useState([]);
     const [loading,setLoading] = useState(false);
 
+    const getFirestore = (path) => {
+        return collection(db, path);
+    }
     const getProducts = async () => {
         setLoading(true);
         try {
-            /*const col = collection(db, 'products');
-            const data = await getDocs(col);
+            /*const data = await getDocs(getFirestore('products'));
             const result = data.docs.map(doc => doc={id:doc.id,...doc.data()});*/
             setProducts(item);
         } catch (error) {
@@ -22,9 +24,16 @@ const UseFirebase = () => {
         }
     }
 
+    const handleSubmit = async (e, form) => {
+        e.preventDefault();
+        const order = await addDoc(getFirestore('orders'), form);
+        console.log(order);
+    }
+
     return {
         getProducts,
-        products
+        products,
+        handleSubmit
     }
 }
-export default UseFirebase
+export default UseFirebase;
