1430bf9b75c2e7f80b05e578e7e2771b490c8f04
Leandro Rey <leandrodrey@gmail.com>
2023-06-02 14:53:54 -0300

    Unused dependencies

--- ecommerce/src/pages/ItemDetailPage.jsx
+++ ecommerce/src/pages/ItemDetailPage.jsx
@@ -1,16 +1,14 @@
 import React, {useContext, useEffect} from 'react'
 import Container from "@mui/material/Container";
-import {useNavigate, useParams} from "react-router-dom";
-import {redirectsTo} from "../helpers";
+import {useParams} from "react-router-dom";
 import {ProductContext} from "../context/ProductProvider";
 import ItemDetail from "../components/ItemDetail/ItemDetail";
 import Typography from "@mui/material/Typography";
 
 const ItemDetailPage = () => {
 
-    const {id} = useParams();
-
     const {product, getProductById} = useContext(ProductContext);
+    const {id} = useParams();
 
     useEffect( () => {
         getProductById(id);
