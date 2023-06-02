e724c79da419bfcf57f493a7089f0e94fbdadee2
Leandro Rey <leandrodrey@gmail.com>
2023-06-02 14:51:01 -0300

    ItemList container

--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
-import ItemListContainer from "../components/ItemListContainer/ItemListContainer";
-import Typography from "@mui/material/Typography";
+import React, {useContext, useEffect} from 'react'
+import {ProductContext} from "../context/ProductProvider";
+import {useParams} from "react-router-dom";
+import ItemList from "../components/ItemList/ItemList";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
-    return (
-        <React.Fragment>
-            <Box>
-                <Typography variant="h3" gutterBottom color="text.secondary">
-                    Hot Items!
-                </Typography>
-            </Box>
-            <ItemListContainer />
-        </React.Fragment>
-    )
+
+    const {products, getProducts, getProductsByCategoryId} = useContext(ProductContext);
+    const {categoryId} = useParams();
+
+    useEffect(() => {
+        {categoryId ? getProductsByCategoryId(categoryId) : getProducts()}
+    }, []);
+
+    if (products) {
+        return (
+            <>
+                <Box>
+                    <Typography variant="h3" gutterBottom color="text.secondary">
+                        Hot Items!
+                    </Typography>
+                </Box>
+                <ItemList products={products} categoryId={categoryId} />
+            </>
+        )
+    } else {
+        return (
+            <Typography variant="h4" gutterBottom color="text.secondary">
+                Products not found
+            </Typography>
+        )
+    }
+
 }
-export default Home
+export default Home;
