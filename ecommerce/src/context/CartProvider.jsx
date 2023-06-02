ceb473628a6982e7e0baf97b10ca9f2deb967208
Leandro Rey <leandrodrey@gmail.com>
2023-06-02 19:56:00 -0300

    checkIfItemExistInCart first approach

--- ecommerce/src/context/CartProvider.jsx
+++ ecommerce/src/context/CartProvider.jsx
@@ -6,7 +6,16 @@ export const CartContext = createContext('')
 const CartProvider = ({children}) => {
 
     const [cart, setCart] = useState([]);
-    const addCart = (item) => setCart([...cart, item]);
+    const addCart = (item) => {
+        if (checkIfItemExistInCart(item.id)) {
+            setCart([...cart, item.count++])
+        } else {
+            setCart([...cart, item])
+        }
+    }
+    const checkIfItemExistInCart = (id) => {
+        return cart.find(item => item.id === id);
+    }
 
     const totalPayments = getTotalPaymentFromCart(cart);
 
