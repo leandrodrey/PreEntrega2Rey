1a3db1f4cb78faa6c15472aeac62b2014078a411
Leandro Rey <leandrodrey@gmail.com>
2023-06-02 23:33:18 -0300

    addCart Updated

--- ecommerce/src/context/CartProvider.jsx
+++ ecommerce/src/context/CartProvider.jsx
@@ -6,13 +6,23 @@ export const CartContext = createContext('')
 const CartProvider = ({children}) => {
 
     const [cart, setCart] = useState([]);
+
     const addCart = (item) => {
         if (checkIfItemExistInCart(item.id)) {
-            setCart([...cart, item.count++])
+            const updatedCart = cart.map((cartItem) => {
+                if (cartItem.id === item.id) {
+                    return {
+                        ...cartItem,
+                        count: cartItem.count + 1
+                    };
+                }
+                return cartItem;
+            });
+            setCart(updatedCart);
         } else {
-            setCart([...cart, item])
+            setCart([...cart, item]);
         }
-    }
+    };
     const checkIfItemExistInCart = (id) => {
         return cart.find(item => item.id === id);
     }
