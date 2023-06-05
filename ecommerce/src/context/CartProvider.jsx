371a8978d710715c374a4e57657c682f132b7999
leandrodrey <leandrodrey@gmail.com>
2023-06-04 22:17:08 -0300

    Updated cart in different method

--- ecommerce/src/context/CartProvider.jsx
+++ ecommerce/src/context/CartProvider.jsx
@@ -9,22 +9,24 @@ const CartProvider = ({children}) => {
 
     const addCart = (item) => {
         if (checkIfItemExistInCart(item.id)) {
-            const updatedCart = cart.map((cartItem) => {
-                if (cartItem.id === item.id) {
-                    return {
-                        ...cartItem,
-                        count: cartItem.count + 1
-                    };
-                }
-                return cartItem;
-            });
-            setCart(updatedCart);
+            setCart(updatedCart(item.id));
         } else {
             setCart([...cart, item]);
         }
     };
+
+    const updatedCart = (id) => cart.map((cartItem) => {
+        if (cartItem.id === id) {
+            return {
+                ...cartItem,
+                count: cartItem.count + 1
+            };
+        }
+        return cartItem;
+    });
+
     const checkIfItemExistInCart = (id) => {
-        return cart.find(item => item.id === id);
+        return cart.some((item) => item.id === id);
     }
 
     const totalPayments = getTotalPaymentFromCart(cart);
