05c0bf2af53f98f17dcf621d4b62475d0e3bf734
leandrodrey <leandrodrey@gmail.com>
2023-06-04 22:16:31 -0300

    Loader and useLoader added

--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -1,18 +1,31 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {ProductContext} from "../context/ProductProvider";
 import {useParams} from "react-router-dom";
 import ItemList from "../components/ItemList/ItemList";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Loader from "../components/Loader/Loader";
+import useLoader from "../hooks/useLoader";
 
 const Home = () => {
 
     const {products, getProducts, getProductsByCategoryId} = useContext(ProductContext);
     const {categoryId} = useParams();
+    const {showLoader, startLoader, stopLoader} = useLoader();
 
     useEffect(() => {
-        {categoryId ? getProductsByCategoryId(categoryId) : getProducts()}
+        startLoader();
+        categoryId ? getProductsByCategoryId(categoryId) : getProducts()
+        setTimeout(() => {
+            stopLoader();
+        }, 1000);
     }, []);
+
+    if (showLoader) {
+        return (
+            <Loader showLoader={true} />
+        )
+    }
 
     if (products) {
         return (
