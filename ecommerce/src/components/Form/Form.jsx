ebbdc708cdb71b1fcd910cd1d84433da25fac324
Leandro Rey <leandrodrey@gmail.com>
2023-06-09 16:22:24 -0300

    Form improvements

--- ecommerce/src/components/Form/Form.jsx
+++ ecommerce/src/components/Form/Form.jsx
@@ -1,11 +1,25 @@
-import React, {useEffect, useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import Button from "@mui/material/Button";
-import {TextField} from "@mui/material";
+import {InputAdornment, TextField} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
+import {LoaderContext} from "../../context/LoaderProvider";
+import {OrderContext} from "../../context/OrderProvider";
+import Loader from "../Loader/Loader";
+import {ProductContext} from "../../context/ProductProvider";
+import {CartContext} from "../../context/CartProvider";
+import './Form.css'
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import PersonIcon from '@mui/icons-material/Person';
+import Typography from "@mui/material/Typography";
+
 
 const Form = (props) => {
 
-    const {total, items, handleSubmit} = props;
+    const {handleSubmit} = useContext(ProductContext);
+    const {cart, getTotalPaymentFromCart, removeAllItemsFromCart} = useContext(CartContext);
+    const {isLoading, stopLoader} = useContext(LoaderContext);
+    const {order} = useContext(OrderContext);
 
     const [form, setForm] = useState({
         buyer: {
@@ -14,9 +28,14 @@ const Form = (props) => {
             lastName: '',
             phone: ''
         },
-        total,
-        items
+        total: getTotalPaymentFromCart(),
+        items: cart
     });
+
+    useEffect(() => {
+        stopLoader();
+    }, []);
+
     const handleChange = (e) => {
         const {name, value} = e.target;
         setForm({
@@ -26,50 +45,109 @@ const Form = (props) => {
                 [name]: value,
             }
         });
-        handleSubmitForm(e);
     }
 
     const handleSubmitForm = (e) => {
-        handleSubmit(e, form);
+        e.preventDefault();
+        handleSubmit(form);
+        removeAllItemsFromCart();
+    }
+
+    if (isLoading) {
+        return (
+            <Loader showLoader={isLoading}/>
+        )
+    }
+
+    console.log(order, 'order in cart');
+    if (order) {
+        return (
+            <div>
+                <h1>Thank you for your purchase!</h1>
+                <p>Your order number is: {order.id}</p>
+            </div>
+        )
     }
 
     return (
-        <form onSubmit={handleChange} action="">
-            <TextField
-                hiddenLabel
-                type="email"
-                name="email"
-                placeholder="Email"
-                variant="filled"
-                size="small"
-            />
-            <TextField
-                hiddenLabel
-                type="text"
-                name="name"
-                placeholder="First Name"
-                variant="filled"
-                size="small"
-            />
-            <TextField
-                hiddenLabel
-                type="text"
-                name="lastName"
-                placeholder="Last Name"
-                id="filled-hidden-label-normal"
-                variant="filled"
-                size="small"
-            />
-            <TextField
-                hiddenLabel
-                type="text"
-                name="phone"
-                placeholder="Phone"
-                variant="filled"
-                size="small"
-            />
-            <Button type="submit" variant="contained" endIcon={<SendIcon/>}>Send</Button>
-        </form>
+        <>
+            <Typography variant="h5" gutterBottom color="text.secondary">
+                Your information:
+            </Typography>
+
+            <form className="order_form" onSubmit={handleSubmitForm} action="">
+                <TextField
+                    onChange={handleChange}
+                    hiddenLabel
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    variant="filled"
+                    size="small"
+                    required
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <AlternateEmailIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <TextField
+                    onChange={handleChange}
+                    hiddenLabel
+                    type="text"
+                    name="phone"
+                    placeholder="Phone"
+                    variant="filled"
+                    size="small"
+                    required
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <PhoneAndroidIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <TextField
+                    onChange={handleChange}
+                    hiddenLabel
+                    type="text"
+                    name="name"
+                    placeholder="First Name"
+                    variant="filled"
+                    size="small"
+                    required
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <PersonIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <TextField
+                    onChange={handleChange}
+                    hiddenLabel
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                    id="filled-hidden-label-normal"
+                    variant="filled"
+                    size="small"
+                    required
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <PersonIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <Button type="submit" variant="contained" endIcon={<SendIcon/>}>Complete Order</Button>
+            </form>
+        </>
     )
 }
 export default Form;
